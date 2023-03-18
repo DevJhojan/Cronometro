b6a4990d0d99a84502b389a749c1eb6ef0913aa0
jdtp125753 <jhojan1806@gmail.com>
2023-03-18 10:03:59 -0500

    Agregando codigo del contador en Consola

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace counter
+{
+    class Program
+    {
+
+        static int minutesRest =30;
+
+        static void Main(string[] args)
+        {
+
+
+            Timer timer = new Timer(60000);
+            timer.AutoReset = true;
+
+
+
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_elapsed);
+
+            timer.Start();
+            Console.WriteLine("Contador iniciado.");
+            Console.ReadLine();
+        }
+
+        private static void timer_elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            minutesRest--;
+            Console.WriteLine($"Quedan {minutesRest} minutos");
+
+            if(minutesRest==0){
+                Console.WriteLine("Tomate la pastilla");
+                ((Timer)sender).Stop();
+            }
+        }
+    }
+}
